a476262041ca605c0308c4cb62339228250c673c
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-11 18:18:24 +0000

    logparsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,46 @@
-#!/usr/in/python3
+#!/usr/bin/python3
 """ Reads stdin line by line and computes metrics """
 
 import sys
 
 
-if __name__ == "__main__":
-    status_code = {
-	       "200": 0,
+st_code = {"200": 0,
                "301": 0,
                "400": 0,
                "401": 0,
                "403": 0,
                "404": 0,
                "405": 0,
-               "500": 0
-	       }
-    count = 0
+               "500": 0}
+    count = 1
     file_size = 0
 
-    def get_file_size(line):
-        """ Get the file size """
-        try:
-            line_arr = line.split()
-            code = line_arr[-2]
-            if code in status_code.keys():
-                status_code[code] += 1
-            return int(line_arr[-1])
+def file_size(line):
+    """ Get the file size of each line """
+    try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
         except Exception:
             return 0
 
-    def print_metrics():
-        """ Print the metrics """
-        print("File size: {}".format(file_size))
-        for key in sorted(status_code.keys()):
-            if status_code[key]:
-                print("{}: {}".format(key, status_code[key]))
+def print_metrics():
+    """ Print the line metrics """
+    print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
+if __name__ == "__main__":
     try:
         for line in sys.stdin:
-	    count += 1
-            file_size += get_file_size(line)
+            file_size += file_size(line)
             if count % 10 == 0:
                 print_metrics()
+            count += 1
     except KeyboardInterrupt:
         print_metrics()
         raise
-    print_metrics()	
+    print_metrics()
