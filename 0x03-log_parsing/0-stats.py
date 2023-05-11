cbec091d9936992d624e679a650eab1b388a8882
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-11 12:37:23 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,7 @@
 import sys
 
 
-status_code = { "200": 0,
+st_code = { "200": 0,
 		"301": 0,
 		"400": 0,
 		"401": 0,
@@ -17,30 +17,29 @@ count = 0
 def file_size(line):
     """ Get the file size of each line """
     try:
-        line_arr = line.split()
-        st_code = line_arr[-2]
-        if st_code in status_code.keys():
-            status_code[st_code] += 1
-            return int(line_arr[-1])
-    except Exception:
-        pass
-    return 0
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
 def print_metrics():
     """ Print the line metrics """
     print("File size: {}".format(file_size))
-    for key in sorted(status_code.keys()):
-        if status_code[key]:
-            print("{}: {}".format(key, status_code[key]))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
-            count += 1
             file_size += file_size(line)
-                if count % 10 == 0:
-                    print_metrics()
-        except KeyboardInterrupt:
-            print_metrics()
-            raise
-	print_metrics()
+            if count % 10 == 0:
+                print_metrics()
+            count += 1
+    except KeyboardInterrupt:
+        print_metrics()
+        raise
+    print_metrics()
