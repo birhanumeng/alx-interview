750598a56b882a02dbf7f53178e0a2b8cb7342c1
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-11 18:45:32 +0000

    logparsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,7 @@
 import sys
 
 
-if __name__ == "__main__":
-    status_code = {"200": 0,
+status_code = {"200": 0,
                "301": 0,
                "400": 0,
                "401": 0,
@@ -13,27 +12,28 @@ if __name__ == "__main__":
                "404": 0,
                "405": 0,
                "500": 0}
-    count = 1
-    file_size = 0
+count = 1
+file_size = 0
 
-    def get_size(line):
-        """ Get file size of each line """
-        try:
-            line_arr = line.split()
-            code = line_arr[-2]
-            if code in status_code.keys():
-                status_code[code] += 1
-            return int(line_arr[-1])
-        except Exception:
-            return 0
+def get_size(line):
+    """ Get file size of each line """
+    try:
+        line_arr = line.split()
+        code = line_arr[-2]
+        if code in status_code.keys():
+            status_code[code] += 1
+        return int(line_arr[-1])
+    except Exception:
+        return 0
 
-    def print_metrics():
-        """ Print line metrics """
-        print("File size: {}".format(file_size))
-        for key in sorted(status_code.keys()):
-            if status_code[key]:
-                print("{}: {}".format(key, status_code[key]))
+def print_metrics():
+    """ Print line metrics """
+    print("File size: {}".format(file_size))
+    for key in sorted(status_code.keys()):
+        if status_code[key]:
+            print("{}: {}".format(key, status_code[key]))
 
+if __name__ == "__main__":
     try:
         for line in sys.stdin:
             file_size += get_size(line)
