b48e9ccdfa4c93a3ef8f6579134c3f02ec3f4bc9
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-11 13:45:58 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,46 @@
-#!/usr/bin/python3
+#!/usr/in/python3
 """ Reads stdin line by line and computes metrics """
 
 import sys
 
 
-st_code = {
-	"200": 0,
-	"301": 0,
-	"400": 0,
-	"401": 0,
-	"403": 0,
-	"404": 0,
-	"405": 0,
-	"500": 0
-	}
-count = 1
-file_size = 0
-
-def parse_line(line):
-    """ Read, parse and grab data"""
-    try:
-        parsed_line = line.split()
-        status_code = parsed_line[-2]
-        if status_code in st_code.keys():
-            st_code[status_code] += 1
-        return int(parsed_line[-1])
-    except Exception:
-        return 0
-
-def print_stats():
-    """print stats in ascending order"""
-    print("File size: {}".format(file_size))
-    for key in sorted(st_code.keys()):
-        if st_code[key]:
-            print("{}: {}".format(key, st_code[key]))
-
 if __name__ == "__main__":
+    status_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 0
+    file_size = 0
+
+    def file_size(line):
+        """ Get the file size """
+        try:
+            line_arr = line.split()
+            code = line_arr[-2]
+            if code in status_code.keys():
+                status_code[code] += 1
+            return int(line_arr[-1])
+        except Exception:
+            return 0
+
+    def print_metrics():
+        """ Print the metrics """
+        print("File size: {}".format(file_size))
+        for key in sorted(status_code.keys()):
+            if status_code[key]:
+                print("{}: {}".format(key, status_code[key]))
 
     try:
         for line in sys.stdin:
-            file_size += parse_line(line)
+	    count += 1
+            file_size += file_size(line)
             if count % 10 == 0:
-                print_stats()
-            count += 1
+                print_metrics()
     except KeyboardInterrupt:
-        print_stats()
+        print_metrics()
         raise
-    print_stats()
+    print_metrics()	
