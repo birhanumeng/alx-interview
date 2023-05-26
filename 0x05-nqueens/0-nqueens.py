0a3737d85c24c4c71ce4624524d7bb7285ca3b0f
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-26 18:09:06 +0000

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Solve the N queens puzzle. """
-
+'''
+Module that solves the N Queens puzzle
+'''
 from sys import argv, exit
-
 
 if __name__ == "__main__":
     if len(argv) != 2:
@@ -17,27 +17,27 @@ if __name__ == "__main__":
         print('N must be at least 4')
         exit(1)
 
-    places = []
+    solution = []
 
-    def solve_queens(row, n, places):
+    def solve_queens(row, n, solution):
         if (row == n):
-            print(places)
+            print(solution)
         else:
             for col in range(n):
-                new = [row, col]
-                if valid_place(places, new):
-                    places.append(new)
-                    solve_queens(row + 1, n, places)
-                    solution.remove(new)
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
 
-    def valid_placement(places, new):
-        for queen in places:
-            if queen[1] == new[1]:
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
                 return False
-            if (queen[0] + queen[1]) == (new[0] + new[1]):
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
                 return False
-            if (queen[0] - queen[1]) == (new[0] - new[1]):
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
                 return False
         return True
 
-    solve_queens(0, n, places)
+    solve_queens(0, n, solution)
