172a388df27dc22a55cb9400321814d953e7baf4
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-05 13:08:33 +0000

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
-request(url, async (error, response, body) => {
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
