7bef6ea568c97ec1ac26c098994e46ff628c38ff
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-08 00:01:53 -0700

    rotate the 2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,10 +6,20 @@
 def rotate_2d_matrix(matrix):
     """ Rotate n x n 2D matrix by 90 degrees clockwise.
     """
-    tmp = matrix
     n = len(matrix)
     index = n - 1
-    for j in range(n):
-        for i in range(n):
-            matrix[i][index] = tmp[j][i]
+
+    # Make a deep copy of the 2D list
+    tmp = []
+    deep_copy = []
+    for i in range(n):
+        for j in range(n):
+            tmp.append(matrix[i][j])
+        deep_copy.append(tmp)
+        tmp = []
+
+    # Rotate the 2D matrix using 2D deep_copy
+    for i in range(n):
+        for j in range(n):
+            matrix[j][index] = deep_copy[i][j]
         index -= 1
