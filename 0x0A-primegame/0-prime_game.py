a3284dc2375583cc029067e0ca20fe8ecd0aa6aa
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-29 13:23:11 +0000

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" Prime game module
+"""
+
+
+def isPrime(n):
+        """ Check if a number in the argument is prime number.
+                - returns true if the argument is prime number
+                - otherwise returns false
+        """
+        for num in range((n // 2) + 1):
+            if n % num == 0:
+                return False
+        return True
+
+
+def checkMultiplies(num, numbers):
+    """ Find out all its multplies for number 'num' in the
+        list of numbers.
+            - num is number
+            - numbers is a list of numbers
+    """
+    multiplies = []
+    for n in numbers:
+        if n % num == 0:
+            multiplies.append(n)
+    return multiplies
+
+
+def isWinner(x, nums):
+    """ Given a set of consecutive integers starting from 1 up
+        to and including n, they take turns choosing a prime number
+        from the set and removing that number and its multiples from
+        the set. The player that cannot make a move loses the game.
+            - x is the number of rounds and nums is an array of n
+            - Return: name of the player that won the most rounds
+            - If the winner cannot be determined, return None
+            - You can assume n and x will not be larger than 10000
+            - n may be diiferen for each round.
+    """
+    if len(nums) == 0 or x < 1:
+        return None
+
+    m_win = 0
+    b_win = 0
+    for i in nums:
+        m_selected = 1
+        b_selected = 0
+        for s in range(1, i + 1):
+            if isPrime(s):
+                if m_selected:
+                    b_selected = 1
+                    m_selected = 0
+                else:
+                    m_selected = 1
+                    b_selected = 0
+        if m_selected:
+            b_win += 1
+        else:
+            m_win += 1
+    if m_win > b_win:
+        return 'Maria'
+    return 'Ben'
